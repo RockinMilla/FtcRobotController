5642af345e967eafddb71a0ea52a1c50f76004fc
RockinMilla <milla.fishler@outlook.com>
2025-09-28 18:00:27 -0700

    This almost works!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveRed.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
+
+@Autonomous(name="Auto Drive Red", group="Robot")
+public class AutoDriveRed extends LinearOpMode {
+    final ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        LinearOpMode o = this;
+        RockinBot r = new RockinBot(o, "Driver");     // Passing in code from RockinBot
+
+        telemetry.addData("Autonomous Ready", "You can press start now");
+
+        telemetry.addData("This code was last updated", "09/25/2025, 5:47 pm"); // Todo: Update this date when the code is updated
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        r.getPinpointPosition();
+        r.driveToPos(500, -500, 0); // backwards, strafe left
+        r.driveToPos(500, -500, -45); // turn towards goal
+        sleep(2000); // pew pew pew
+        r.driveToPos(500, -600, -45); // strafe right
+        r.driveToPos(1000, -600, -90); // turn right
+        r.driveToPos(1000, -200, -90); // forward (intake balls)
+        sleep(1000); // slurp up balls
+        r.driveToPos(500, -500, -90); // backward
+        r.driveToPos(500, -500, -45); // turn towards goal
+        sleep(2000); // pew pew pew
+        r.driveToPos(500, -1300, -45); // strafe right
+        r.driveToPos(500, -1300, -90); // turn right
+        r.driveToPos(200, -1300, -90); // forward (intake balls)
+        sleep(1000); // slurp up balls
+        r.driveToPos(500, -500, -90); // backward, strafe left
+        r.driveToPos(500, -500, -45); // turn towards goal
+        sleep(2000); // pew pew pew
+        // turn towards gate
+        // park ready to activate gate
+        RobotLog.vv("Rockin' Robots", "Test Done");
+        telemetry.addData("Autonomous lasted", runtime.toString());
+        sleep(4 * 1000);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RockinBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RockinBot.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.RobotLog;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
