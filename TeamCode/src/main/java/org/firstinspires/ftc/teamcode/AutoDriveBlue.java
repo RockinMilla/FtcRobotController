a83ea55aaa7b9603f09c6fbd56b0d709f8c384a4
RockinMilla <milla.fishler@outlook.com>
2025-10-02 17:33:12 -0700

    This works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveBlue.java
@@ -16,29 +16,29 @@ public class AutoDriveBlue extends LinearOpMode {
 
         telemetry.addData("Autonomous Ready", "You can press start now");
 
-        telemetry.addData("This code was last updated", "09/28/2025, 4:56 pm"); // Todo: Update this date when the code is updated
+        telemetry.addData("This code was last updated", "10/2/2025, 5:32 pm"); // Todo: Update this date when the code is updated
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         r.getPinpointPosition();
         r.driveToPos(500, 500, 0); // backwards, strafe left
         r.driveToPos(500, 500, 45); // turn towards goal
-        sleep(2000); // pew pew pew
+        sleep(500); // pew pew pew
         r.driveToPos(500, 600, 45); // strafe right
         r.driveToPos(1000, 600, 90); // turn right
         r.driveToPos(1000, 200, 90); // forward (intake balls)
-        sleep(1000); // slurp up balls
+        sleep(500); // slurp up balls
         r.driveToPos(500, 500, 90); // backward
         r.driveToPos(500, 500, 45); // turn towards goal
-        sleep(2000); // pew pew pew
+        sleep(500); // pew pew pew
         // correct up to here
         r.driveToPos(500, 600, 45); // strafe right
-        r.driveToPos(500, 600, 90); // turn right
-        r.driveToPos(200, 200, 90); // forward (intake balls)
-        sleep(1000); // slurp up balls
+        r.driveToPos(1600, 600, 90); // turn right
+        r.driveToPos(1600, 200, 90); // forward (intake balls)
+        sleep(500); // slurp up balls
         r.driveToPos(500, 500, 90); // backward, strafe left
         r.driveToPos(500, 500, 45); // turn towards goal
-        sleep(2000); // pew pew pew
+        sleep(500); // pew pew pew
         // turn towards gate
         // park ready to activate gate
         RobotLog.vv("Rockin' Robots", "Test Done");
