b8d6d1031bc8b171552c539087d6fb9493675ce4
RockinMilla <milla.fishler@outlook.com>
2025-10-16 15:50:36 -0700

    This does not work!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveRed.java
@@ -18,25 +18,25 @@ public class AutoDriveRed extends LinearOpMode {
 
         telemetry.addData("This code was last updated", "10/2/2025, 5:32 pm"); // Todo: Update this date when the code is updated
 
-        // Wait for the game to start (driver presses PLAY)
+        // Wait for the game to start (driver presses PLAY) TODO: switch x and y pp
         waitForStart();
         r.getPinpointPosition();
-        r.driveToPos(500, -500, 0); // backwards, strafe left
-        r.driveToPos(500, -500, -45); // turn towards goal
+        r.driveToPos(-500, -500, 0); // backwards, strafe left
+        r.driveToPos(-500, -500, -45); // turn towards goal
         sleep(500); // pew pew pew
-        r.driveToPos(500, -600, -45); // strafe right
-        r.driveToPos(1000, -600, -90); // turn right
-        r.driveToPos(1000, -200, -90); // forward (intake balls)
+        r.driveToPos(-500, -600, -45); // strafe right
+        r.driveToPos(-1000, -600, -90); // turn right
+        r.driveToPos(-1000, -200, -90); // forward (intake balls)
         sleep(500); // slurp up balls
-        r.driveToPos(500, -500, -90); // backward
-        r.driveToPos(500, -500, -45); // turn towards goal
+        r.driveToPos(-500, -500, -90); // backward
+        r.driveToPos(-500, -500, -45); // turn towards goal
         sleep(500); // pew pew pew
-        r.driveToPos(1600, -500, -45); // strafe right
-        r.driveToPos(1600, -500, -90); // turn right
-        r.driveToPos(1600, -200, -90); // forward (intake balls)
+        r.driveToPos(-1600, -500, -45); // strafe right
+        r.driveToPos(-1600, -500, -90); // turn right
+        r.driveToPos(-1600, -200, -90); // forward (intake balls)
         sleep(500); // slurp up balls
-        r.driveToPos(500, -500, -90); // backward, strafe left
-        r.driveToPos(500, -500, -45); // turn towards goal
+        r.driveToPos(-500, -500, -90); // backward, strafe left
+        r.driveToPos(-500, -500, -45); // turn towards goal
         sleep(500); // pew pew pew
         // turn towards gate
         // park ready to activate gate
