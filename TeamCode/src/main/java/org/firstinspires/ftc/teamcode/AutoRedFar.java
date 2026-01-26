6974e005ae095780cd09fa6d797b6adde6f3a773
RockinMilla <milla.fishler@outlook.com>
2026-01-26 13:07:17 -0800

    working on auto -- reliable 12 ball both sides!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedFar.java
@@ -15,7 +15,7 @@ public class AutoRedFar extends LinearOpMode {
         RockinBot r = new RockinBot(o, "Shooter");     // Passing in code from RockinBot
 
         telemetry.addData("Autonomous Ready", "You can press start now");
-            telemetry.addData("This code was last updated", "1/10/2026, 2:56 pm"); // Todo: Update this date when the code is updated
+        telemetry.addData("This code was last updated", "1/26/2026, 12:53 pm"); // Todo: Update this date when the code is updated
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -28,20 +28,22 @@ public class AutoRedFar extends LinearOpMode {
 
         shootBalls(r); // Shoot preloaded balls
 
-        r.driveToPos(500, 200, -70); // park
+        r.driveToPos(400, 200, -45); // park
         r.launcherVelocity(0);
         r.intakePower(0);
     }
 
-    private void shootBalls(RockinBot r)
-    {
+    private void shootBalls(RockinBot r) {
         r.intakePower(0.2);
-        r.driveToPos(0, 100, -20, 10, 1.5 ,5);
+        r.driveToPos(0, 100, -20, 10, 1.5, 5);
         r.waitForLaunchers(1200); // Change this when changing launcher velocity
         r.intakePower(0.5);
-        r.turnLifterToDegrees(360); // Shoot the balls
+        r.turnLifterToDegrees(90); // Shoot ball 1
+        sleep(500);
+        r.turnLifterToDegrees(180); // Shoot ball 2
+        sleep(500);
+        r.turnLifterToDegrees(270); // Shoot ball 3
         r.waitForLifter();
-        r.turnLifterToDegrees(-190); // Reset lifter
         sleep(1000);
     }
 }
